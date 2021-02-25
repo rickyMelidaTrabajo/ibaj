c3e33a779709cbf904e27db437d57a605c26d911
Ricardo Mélida <ricardomelida92@gmail.com>
2021-02-25 11:59:22 -0300

    Se agrega servicio de Firebase Storage
    Se ha agregado servicio para poder almacenar las imagenes desde el
    storage de firebase.
    Falataria solo poder agregar los metodos de dicha accion y vincularlas
    a las paginas correspondientes.

--- src/app/components/activities/slider-activities/slider-activities.component.ts
+++ src/app/components/activities/slider-activities/slider-activities.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Activities } from 'src/app/models/activities.interface';
+import { ActivitiesServicesService } from '../../../services/activities-services.service';
+import { rejects } from 'assert';
+import { FirebaseStorageServiceService } from '../../../services/firebase-storage-service.service';
+
 
 @Component({
   selector: 'app-slider-activities',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderActivitiesComponent implements OnInit {
 
-  constructor() { }
+  images: Array<any> = new Array();
+  activities: Array<Activities>;
+  constructor( private _activitiesService: ActivitiesServicesService, private imagesStorage: FirebaseStorageServiceService) { }
 
   ngOnInit(): void {
+    this.getData()
+    .then((result) => {
+      this.activities = result
+      this.activities.forEach((value)=>{
+        this.images.push(value.imagen);
+      });
+    });
+
+    this.getImages()
+    .then(res=>{
+      console.log(res);
+    }).catch(err=>{
+
+    })
+  }
+
+  getData(): any {
+    return new Promise((resolve, reject) => {
+      this._activitiesService.getActivities().subscribe((item) => {
+        let datos = new Array;
+        item.forEach(element => {
+          datos.push(element.payload.doc.data());
+          // this.id.push(element.payload.doc.id);
+          resolve(datos);
+        });
+      });
+    });
+  }
+
+  getImages() {
+    return new Promise((resolve, rejects)=>{
+      this.imagesStorage.getActivities();
+      resolve(this.imagesStorage.getActivities());
+    })
   }
 
 }
